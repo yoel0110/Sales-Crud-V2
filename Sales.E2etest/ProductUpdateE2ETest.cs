3b3e970a363d168f6dfcaa7fed9177cf9f0cf18a
Yoel 4Devs <Yoellebron35@gmail.com>
2026-08-05 02:51:01 -0400

    feat: HU-006: Editar producto con precio maximo

--- Sales.E2etest/ProductUpdateE2ETest.cs
+++ Sales.E2etest/ProductUpdateE2ETest.cs
@@ -64,4 +64,29 @@ public class ProductUpdateE2ETest : TestBase
         Assert.That(form.Displayed, Is.True);
         Test.Pass("Actualizacion con nombre vacio rechazada");
     }
+
+    [Test]
+    [Category("Prueba de limites")]
+    public void Update_Boundary_MaxPrice()
+    {
+        Test.Info("Esperando lista de productos");
+        LoginHelper.WaitUntilElementVisible(_driver, By.CssSelector(".product-table"), TimeSpan.FromSeconds(5));
+
+        Test.Info("Seleccionando primer producto para editar");
+        var editButton = _driver.FindElement(By.CssSelector("button[id^='edit-product-']"));
+        editButton.Click();
+        LoginHelper.WaitUntilElementExists(_driver, By.Id("productName"), TimeSpan.FromSeconds(3));
+
+        Test.Info("Actualizando con precio maximo");
+        var priceInput = _driver.FindElement(By.Id("price"));
+        priceInput.Clear();
+        priceInput.SendKeys("999999.99");
+        _driver.FindElement(By.Id("add")).Click();
+
+        Test.Info("Verificando mensaje de respuesta");
+        LoginHelper.WaitUntilElementVisible(_driver, By.Id("response-modal-message"), TimeSpan.FromSeconds(5));
+        var message = _driver.FindElement(By.Id("response-modal-message")).Text;
+        Assert.That(message, Is.Not.Empty);
+        Test.Pass("Precio maximo procesado");
+    }
 }
